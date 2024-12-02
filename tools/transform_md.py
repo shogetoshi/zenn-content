9322693a7dc98f460d0a6c00c9aee97ab3e3fc47
shogetoshi <188074504+shogetoshi@users.noreply.github.com>
2024-12-03 01:05:42 +0900

    preview環境

---
+++ tools/transform_md.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import re
+import sys
+
+
+def delete_newline(lines):
+    in_code = False
+    new_lines = []
+    for i in range(len(lines)):
+        if i == len(lines) - 1:
+            new_lines.append(lines[i])
+            break
+
+        curr_line = lines[i]
+        next_line = lines[i + 1]
+        if curr_line.startswith(("```", "---")):
+            in_code = not in_code
+            new_lines.append(curr_line)
+            continue
+        if curr_line.strip() == "":
+            new_lines.append(curr_line)
+            continue
+        if next_line.strip() == "":
+            new_lines.append(curr_line)
+            continue
+
+        if in_code:
+            new_lines.append(curr_line)
+        else:
+            if re.match(r"(#+ |- |\* |\d+\. |https?://\S+\s*$)", curr_line):
+                new_lines.append(curr_line)
+            elif re.match(r"(#+ |- |\* |\d+\. |```|https?://\S+\s*$)", next_line):
+                new_lines.append(curr_line)
+            else:
+                new_lines.append(curr_line[:-1] + " ")
+    return new_lines
+
+
+def insert_newline(lines):
+    in_code = False
+    new_lines = []
+    for i in range(len(lines)):
+        if i == len(lines) - 1:
+            new_lines.append(lines[i])
+            break
+
+        curr_line = lines[i]
+        next_line = lines[i + 1]
+
+        if in_code:
+            new_lines.append(curr_line)
+        else:
+            if re.match(r"(- |\d+\. )", curr_line):
+                new_lines.append(curr_line)
+                if not re.match(r"(#+ |- |\* |\d+\. |```|https?://\S+\s*$)", next_line):
+                    new_lines.append("\n")
+            else:
+                new_lines.append(curr_line)
+    return new_lines
+
+
+def adjust_heading(lines):
+    in_code = False
+    new_lines = []
+    for i in range(len(lines)):
+        curr_line = lines[i]
+
+        if curr_line.startswith("```"):
+            in_code = not in_code
+
+        if in_code:
+            new_lines.append(curr_line)
+        else:
+            if re.match(r"#+ ", curr_line):
+                new_lines.append("#" + curr_line)
+            else:
+                new_lines.append(curr_line)
+    return new_lines
+
+
+if __name__ == "__main__":
+    lines = sys.stdin.readlines()
+
+    lines = delete_newline(lines)
+    lines = insert_newline(lines)
+    lines = adjust_heading(lines)
+
+    print("".join(lines))
