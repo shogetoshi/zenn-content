b391d7c973e95c4c094621f249676d0ebdcf395e
shogetoshi <moc.7iamg@gmail.com>
2024-12-03 01:05:42 +0900

    preview環境

---
+++ tools/preview.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Copy from $SOURCE_DIR"
+
+BASE_DIR="$(dirname $(dirname $0))"
+
+function copy() {
+    while true; do
+        for path in "$SOURCE_DIR"/*; do
+            basename=$(basename "$path")
+            sync="${BASE_DIR}/sync/${basename}"
+            dest="${BASE_DIR}/articles/${basename#*-}"
+            if ! cmp -s ${path} ${sync}; then
+                echo sync ${path} to ${dest}
+                cp ${path} ${sync}
+                cat ${sync} | python ${BASE_DIR}/tools/transform_md.py > ${dest}
+            fi
+        done
+        sleep 1
+        echo -n '.'
+    done
+}
+
+npx zenn preview --open &
+
+copy &
+
+cat
---
+++ tools/test_transform_md.py
@@ -0,0 +1,58 @@
+import pytest
+import transform_md as target
+
+
+@pytest.mark.parametrize(
+    "lines,expected",
+    [
+        (["aaa\n", "bbb\n"], ["aaa ", "bbb\n"]),
+        (["aaa\n", "\n", "bbb\n"], ["aaa\n", "\n", "bbb\n"]),
+        (["## aaa\n", "bbb\n"], ["## aaa\n", "bbb\n"]),
+        (["1. aaa\n", "bbb\n"], ["1. aaa\n", "bbb\n"]),
+        (["aaa\n", "## bbb\n"], ["aaa\n", "## bbb\n"]),
+        (
+            ["```python\n", "aaa\n", "bbb\n", "```\n", "aaa\n", "bbb\n"],
+            ["```python\n", "aaa\n", "bbb\n", "```\n", "aaa ", "bbb\n"],
+        ),
+        (
+            ["```aaa\n", "https://www.google.co.jp \n", "bbb\n"],
+            ["```aaa\n", "https://www.google.co.jp \n", "bbb\n"],
+        ),
+    ],
+)
+def test_delete_newlines(lines, expected):
+    response = target.delete_newline(lines)
+    assert response == expected
+
+
+@pytest.mark.parametrize(
+    "lines,expected",
+    [
+        (["- aaa\n", "bbb\n"], ["- aaa\n", "\n", "bbb\n"]),
+        (
+            ["---\n", "title\n"],
+            ["---\n", "title\n"],
+        ),
+    ],
+)
+def test_insert_newline(lines, expected):
+    response = target.insert_newline(lines)
+    assert response == expected
+
+
+@pytest.mark.parametrize(
+    "lines,expected",
+    [
+        (
+            ["# aaa\n"],
+            ["## aaa\n"],
+        ),
+        (
+            ["```\n", "# aaa\n"],
+            ["```\n", "# aaa\n"],
+        ),
+    ],
+)
+def test_adjust_heading(lines, expected):
+    response = target.adjust_heading(lines)
+    assert response == expected
